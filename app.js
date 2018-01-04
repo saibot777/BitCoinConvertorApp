59acb5dff61bccb01d50ee52b1c02ab9b9335cd0
saibot777 <stefantrajkovic777@gmail.com>
2018-01-04 12:44:52 +0100

    express implmented, basic UI added

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const _http = require('http');
-const _request = require('request');
+const express = require('express');
+let app = express();
+const request = require('request');
 
-_http.createServer((req, res) => {
-    console.log('Server started...')
-}).listen(3000);
+request(
+    {
+        url: "https://blockchain.info/stats?format=json",
+        json: true
+    }, (error, response, body) => {
+        btcPrice = body.market_price_usd;
+        btcBlocks = body.n_blocks_total;
+    }
+);
+
+app.get('/', (req, res) => {
+    res.send('Bitcoin rules');
+});
+
+app.get('/block', (req, res) => {
+    res.sendfile("index.html");
+})
+
+app.listen(3000, () => {
+    console.log('Server is running...');
+})
